e400d0ef9b3e1634ab8b1979bc31372304c89eb1
as.lizin <as.lizin@tensor.ru>
2022-03-27 14:24:25 +0700

    * Доабвлено решение для Palindrome Number

--- Palindrome_Number/main.cpp
+++ Palindrome_Number/main.cpp
@@ -8,123 +8,45 @@
 
 using namespace std;
 
-template <class T>
-ostream& operator << (ostream& os, const vector<T>& s) {
-   os << "{";
-   bool first = true;
-   for (const auto& x : s) {
-      if (!first) {
-         os << ", ";
-      }
-      first = false;
-      os << x;
-   }
-   return os << "}";
-}
-
-template <class T>
-ostream& operator << (ostream& os, const set<T>& s) {
-   os << "{";
-   bool first = true;
-   for (const auto& x : s) {
-      if (!first) {
-         os << ", ";
-      }
-      first = false;
-      os << x;
-   }
-   return os << "}";
-}
-
-template <class K, class V>
-ostream& operator << (ostream& os, const map<K, V>& m) {
-   os << "{";
-   bool first = true;
-   for (const auto& kv : m) {
-      if (!first) {
-         os << ", ";
-      }
-      first = false;
-      os << kv.first << ": " << kv.second;
-   }
-   return os << "}";
-}
-
-template<class T, class U>
-void AssertEqual(const T& t, const U& u, const string& hint = {}) {
-   if (t != u) {
-      ostringstream os;
-      os << "Assertion failed: " << t << " != " << u;
-      if (!hint.empty()) {
-         os << " hint: " << hint;
-      }
-      throw runtime_error(os.str());
-   }
-}
-
-void Assert(bool b, const string& hint) {
-   AssertEqual(b, true, hint);
-}
-
-class TestRunner {
+class Solution {
 public:
-   template <class TestFunc>
-   void RunTest(TestFunc func, const string& test_name) {
-      try {
-         func();
-         cerr << test_name << " OK" << endl;
-      } catch (exception& e) {
-         ++fail_count;
-         cerr << test_name << " fail: " << e.what() << endl;
-      } catch (...) {
-         ++fail_count;
-         cerr << "Unknown exception caught" << endl;
+   bool isPalindrome(int x) {
+      if (x < 0)
+      {
+         return false;
       }
-   }
 
-   ~TestRunner() {
-      if (fail_count > 0) {
-         cerr << fail_count << " unit tests failed. Terminate" << endl;
-         exit(1);
+      std::vector<int> decomposition;
+
+      int tmp = x;
+      while(tmp > 0)
+      {
+         decomposition.push_back(tmp % 10);
+         tmp = tmp / 10;
       }
-   }
 
-private:
-   int fail_count = 0;
+      bool result = true;
+
+      size_t size = decomposition.size();
+      for (size_t i=0; i<decomposition.size()/2; ++i)
+      {
+         auto a = decomposition[i];
+         auto b = decomposition[size-1-i];
+         if (a != b)
+         {
+            result = false;
+            break;
+         }
+      }
+
+      return result;
+   }
 };
 
-double discreminant(double a, double b, double c)
-{
-   return b*b-4*a*c;
-}
-
-int GetDistinctRealRootCount(double a, double b, double c)
-{
-   auto d = discreminant(a,b,c);
-   if ( d > 0.0)
-   {
-      return 2.0;
-   }
-   else if (d == 0.0)
-   {
-      return 1.0;
-   }
-   else
-   {
-      return 0.0;
-   }
-}
-
-void Test()
-{
-   AssertEqual( GetDistinctRealRootCount(1,30,4), 2, "2");
-   AssertEqual( GetDistinctRealRootCount(4,2,1), 0, "0");
-
-}
-
 int main() {
-   TestRunner runner;
-   runner.RunTest(Test, "Test");
-   // добавьте сюда свои тесты
+   Solution solution;
+   std::cout << (solution.isPalindrome(123321) == true) << std::endl;
+   std::cout << (solution.isPalindrome(121) == true) << std::endl;
+   std::cout << (solution.isPalindrome(123) == false) << std::endl;
    return 0;
 }
